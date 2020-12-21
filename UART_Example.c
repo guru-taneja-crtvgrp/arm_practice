cb2990de8054f7ba5fd16f30d4ca9ed5d755fd2a
guru-taneja-crtvgrp <73058408+guru-taneja-crtvgrp@users.noreply.github.com>
2020-12-21 22:16:52 +0530

    changed delay and display string.

--- UART_Example.c
+++ UART_Example.c
@@ -1,30 +1,46 @@
 #include<LPC214X.h>
-char rx[5],l=0;
-__irq void uart_isr(void)
-{
-	int iir_value;
-	iir_value = U0IIR;
-	IO0CLR = 0x00000004;
-		VICVectAddr = 0x00;
-	//while ( !(iir_value & 0x01) );
-//	if( iir_value & 0x00000004 )
-//	{
-//		rx[l] = U0RBR;
-//	}
-//	U0THR = rx[l];
-//	l++;
-	//while( (U0LSR & 0x40) == 0 );
-	//memset((void *)U0THR,0,4);
-
-}
+char rx[10];
+unsigned int l;
 void uart_init(void)
 {
 	PINSEL0 = PINSEL0 | 0x00000005;	/* Enable UART0 Rx0 and Tx0 pins of UART0 */
 	U0LCR = 0x83;	                  /* DLAB = 1, 1 stop bit, 8-bit character length */
 	U0DLM = 0x00;	                  /* For baud rate of 9600 with Pclk = 12MHz */
 	U0DLL = 0x4E;	                  /* We get these values of U0DLL and U0DLM from formula */
-	U0LCR = 0x03;                   /* DLAB = 0 */
+	U0LCR = 0x03;  
+  U0TER = 0x80;   	/* DLAB = 0 */
 	U0IER = 0x00000003;	            /* Enable THRE and RBR interrupt */
+}
+void delay()
+{
+T0TCR=0X02;
+T0CTCR=0X00;
+T0PR=0X1D;
+T0MR0=500000;
+T0MCR=0X0006;
+	T0TCR=0X01;
+while(T0TC<500000);
+T0TCR=0X00;
+}
+__irq void uart_isr(void)
+{
+   
+	int iir_value;
+  iir_value = U0IIR;
+	if( iir_value & 0x00000004 )
+	{
+		rx[l] = U0RBR;
+		U0THR = rx[l];
+	}
+      l++;
+	   //U0IIR=0x01;
+	iir_value = U0IIR;
+	if(iir_value==0X03)
+	{
+		IO0CLR=(1<<15);
+	}
+	VICVectAddr = 0x00;
+	
 }
 void interrupt_init(void)
 {
@@ -33,6 +49,7 @@ void interrupt_init(void)
 	VICIntEnable = 0x00000040;	/* Enable UART0 interrupt */
 	VICIntSelect = 0x00000000;	/* UART0 configured as IRQ */
 }
+
 void pll_init(void)
 {
 	PLL0CON = 0x01;
@@ -50,27 +67,27 @@ void delay()
 T0TCR=0X02;
 T0CTCR=0X00;
 T0PR=0X1D;
-T0MR0=10000000;
+T0MR0=500000;
 T0MCR=0X0006;
 	T0TCR=0X01;
-while(T0TC<10000000);
+while(T0TC<500000);
 T0TCR=0X00;
 }
 int main(void)
 {
-	IO0DIR = 0x00000004;
-	unsigned char i[] = {'a','b','c','d','e'};
-	unsigned int j,k;
+	IO0DIR=0X0000FF00;
+	IO0SET=(1<<15);
 	pll_init();
 	uart_init();
   interrupt_init();
-	for (j=0;j<5;j++)
-	{
-	    U0THR = i[j];
-		  IO0SET = 0x00000004;
-		  delay();
-	}
+	U0THR = 'A';
+
+		U0THR = 'b';
+
+		U0THR = 'c';
+
+		U0THR = 'd';
 	
-	//U0THR = i[0];
+		U0THR = 'e';
 	while(1);
 }
